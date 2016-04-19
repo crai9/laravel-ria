c8c0c96301482bde5ca5f52f14e5b5730d913158
Craig Miller <craigm1357@gmail.com>
2016-04-19 11:20:02 +0100

    fix variable typo

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -16,7 +16,7 @@ class ProductTableSeeder extends Seeder
 
         for ($i = 0; $i < 100; $i++)
         {
-            $user = Product::create(array(
+            $product = Product::create(array(
                 'name' => $faker->safeColorName . ' ' . $faker->randomElement(['shirt', 'hat', 'shoes', 'jacket', 'bag', 'tie', 'glasses']),
                 'desc' => $faker->sentence(7, true),
                 'price' => $faker->randomFloat(2, 5, 50),
