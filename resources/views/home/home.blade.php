cd98681b0a2785cd9c02e0de461d786c7dd4386b
Craig Miller <craigm1357@gmail.com>
2016-04-19 10:41:13 +0100

    update images on carousell

--- resources/views/home/home.blade.php
+++ resources/views/home/home.blade.php
@@ -7,40 +7,40 @@
             Finally Navigate to the basket and press checkout.</p>
     </div>
 </div>
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+<div id="change" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        <li data-target="#change" data-slide-to="0" class="active"></li>
+        <li data-target="#change" data-slide-to="1"></li>
+        <li data-target="#change" data-slide-to="2"></li>
     </ol>
 
     <div class="carousel-inner" role="listbox">
         <div class="item active">
-            <img src="http://lorempixel.com/1000/300/fashion/?3" alt="...">
+            <img src="/img/one.jpeg" alt="...">
             <div class="carousel-caption">
-                Pic
+                Image one
             </div>
         </div>
         <div class="item">
-            <img src="http://lorempixel.com/1000/300/fashion/?2" alt="...">
+            <img src="/img/two.jpeg" alt="...">
             <div class="carousel-caption">
-                Pic
+                Image two
             </div>
         </div>
         <div class="item">
-            <img src="http://lorempixel.com/1000/300/fashion/?1" alt="...">
+            <img src="/img/three.jpeg" alt="...">
             <div class="carousel-caption">
-                Pic
+                Image three
             </div>
         </div>
     </div>
 
     <!-- Controls -->
-    <a class="left carousel-control vert-center-content" href="#carousel-example-generic" role="button" data-slide="prev">
+    <a class="left carousel-control vert-center-content" href="#change" role="button" data-slide="prev">
         <i class="fa fa-chevron-circle-left" aria-hidden="true"></i>
         <span class="sr-only">Previous</span>
     </a>
-    <a class="right carousel-control vert-center-content" href="#carousel-example-generic" role="button" data-slide="next">
+    <a class="right carousel-control vert-center-content" href="#change" role="button" data-slide="next">
         <i class="fa fa-chevron-circle-right" aria-hidden="true"></i>
         <span class="sr-only">Next</span>
     </a>
